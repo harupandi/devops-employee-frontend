7da62990a4a6006f3014101402607bb5f0843d8f
Bryan Pineda <bryan.pinedag@hotmail.com>
2026-09-22 17:06:56 -0600

    added versioning for testing purposes

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
         <header className="hero">
           <div>
             <p className="eyebrow">DEVOPS DEMO</p>
-            <h1>Employee Directory</h1>
+            <h1>Employee Directory v1</h1>
             <p className="subtitle">
               React frontend consuming a RESTful Flask API.
             </p>
